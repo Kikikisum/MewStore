64a64aa8a19acf226baac2d2667553278082f711
Kikikisum <2313810210@qq.com>
2023-04-30 00:03:13 +0800

    收藏表的实体类

---
+++ src/main/java/com/example/Entity/Favorite.java
@@ -0,0 +1,17 @@
+package com.example.Entity;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Data
+@ToString
+@AllArgsConstructor
+@NoArgsConstructor
+@TableName(value = "favorite")
+public class Favorite {
+    private Long user_id;
+    private Long good_id;
+}
