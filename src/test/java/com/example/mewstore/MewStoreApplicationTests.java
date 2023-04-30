411df0a2a6d08d0c84ee426b97ec9a9b85af2a3c
Kikikisum <2313810210@qq.com>
2023-04-30 17:01:32 +0800

    测试文件

---
+++ src/test/java/com/example/mewstore/MewStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mewstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MewStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
