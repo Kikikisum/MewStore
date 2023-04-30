e29490ca58f149c1120a8c9b027ef39d819e4f41
Kikikisum <2313810210@qq.com>
2023-04-30 16:02:34 +0800

    收藏商品的操作

--- src/main/java/com/example/Controller/FavoriteController.java
+++ src/main/java/com/example/Controller/FavoriteController.java
@@ -1,12 +1,15 @@
 package com.example.Controller;
 
 import com.alibaba.fastjson.JSON;
+import com.example.Entity.*;
 import com.example.Entity.Favorite;
-import com.example.Entity.Order;
-import com.example.Entity.User;
+import com.example.Service.FavoriteService;
+import com.example.Service.GoodService;
+import com.example.Service.UserService;
 import com.example.Util.DecodeJwtUtils;
 import com.example.Util.SnowFlakeUtil;
 import jakarta.servlet.http.HttpServletRequest;
+import org.apache.ibatis.jdbc.Null;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -22,19 +25,18 @@ public class FavoriteController {
     Map<String, Object> map=new HashMap<>();
     @Autowired
     private DecodeJwtUtils decodeJwtUtils;
+    private UserService userService;
+    private FavoriteService favoriteService;
+    private GoodService goodService;
 
     @PostMapping("fav/{id}")
     public String fav(HttpServletRequest request, @PathVariable("id") Long good_id)
     {
         String token = request.getHeader("token");
         Long uid = Long.valueOf(decodeJwtUtils.getId(token));
-        int user_status = Integer.parseInt(decodeJwtUtils.getStatus(token));
+        User user=userService.getUserById(uid);
+        int user_status = user.getStatus();
         if(decodeJwtUtils.validity(token))
-        {
-            map.put("code",401);
-            map.put("msg","登录过期");
-        }
-        else
         {
             if(user_status==1)
             {
@@ -48,8 +50,25 @@ public class FavoriteController {
             }
             else
             {
-
+                Good good=goodService.queryById(good_id);
+                if(good!= null)
+                {
+                    Favorite favorite=new Favorite(uid,good_id);
+                    favoriteService.InsertFavorite(favorite);
+                    map.put("code",200);
+                    map.put("msg","收藏成功");
+                }
+                else
+                {
+                    map.put("code",404);
+                    map.put("msg","商品不存在");
+                }
             }
+        }
+        else
+        {
+            map.put("code",401);
+            map.put("msg","登录过期");
         }
         return JSON.toJSONString(map);
     }
